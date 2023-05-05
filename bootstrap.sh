fc06cd7ec01671499c299f48ff3aeea984e442c7
JungWKim <lionmini@naver.com>
2023-05-06 02:57:49 +0900

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -49,9 +49,20 @@ sed -i "s/#mgmt01/mgmt01/g" config/inventory
 sed -i "s/10.0.0.1/${IP}/g" config/inventory
 sed -i'' -r -e "/\[kube-node\]/a\mgmt01" config/inventory
 
+# disable changing hostnames of the hosts following as the inventory
+#sed -i "s/deepops_set_hostname: true/deepops_set_hostname: false/g" config/group_vars/all.yml
+
 # activate container level nvidia driver(default) then no reboot will be occurred
 # if below command is commented, host level nvidia driver 515.105.01 will be installed and reboot will also occur. Then you must rerun ansible-playbook one more time after reboot
 sed -i "s/gpu_operator_preinstalled_nvidia_software: true/gpu_operator_preinstalled_nvidia_software: false/g" config/group_vars/k8s-cluster.yml
+
+# force install NVIDIA driver even if GPU not detected
+#sed -i "s/nvidia_driver_force_install: false/nvidia_driver_force_install: true/g" config/group_vars/all.yml
+
+# change cri from containerd to docker
+#sed -i "s/container_manager: containerd/container_manager: docker/g" config/group_vars/k8s-cluster.yml
+# install docker latest version
+#sed -i "s/docker_version: '20.10'/docker_version: 'latest'/g" config/group_vars/all.yml  
 
 # disable nfs provisioner
 sed -i "s/k8s_nfs_client_provisioner: true/k8s_nfs_client_provisioner: false/g" config/group_vars/k8s-cluster.yml
@@ -62,6 +73,15 @@ sed -i "s/k8s_nfs_client_provisioner: true/k8s_nfs_client_provisioner: false/g" 
 #sed -i "s/{{ groups\[\"kube-master\"\]\[0\] }}/${NFS_IP}/g" config/group_vars/k8s-cluster.yml
 #sed -i 's:\/export\/deepops_nfs:${NFS_PATH}:g' config/group_vars/k8s-cluster.yml
 
+# install extra software packages
+sed -i "s/#software_extra_packages:/software_extra_packages:/g" config/group_vars/all.yml
+sed -i "s/#  - curl/  - curl/g" config/group_vars/all.yml
+sed -i "s/#  - git/  - git/g" config/group_vars/all.yml
+sed -i "s/#  - tmux/  - tmux/g" config/group_vars/all.yml
+sed -i "s/#  - vim/  - vim/g" config/group_vars/all.yml
+sed -i "s/#  - wget/  - wget/g" config/group_vars/all.yml
+sed -i "s/#  - build-essential/  - build-essential/g" config/group_vars/all.yml
+
 # deploy k8s
 ansible-playbook -l k8s-cluster playbooks/k8s-cluster.yml -K
 # deploy nfs provisioner manually
@@ -70,3 +90,6 @@ ansible-playbook -l k8s-cluster playbooks/k8s-cluster.yml -K
 ./scripts/k8s/deploy_dashboard_user.sh
 # deploy monitoring(prometheus and grafana)
 ./scripts/k8s/deploy_monitoring.sh
+
+# enable to access ngc
+#kubectl create secret docker-registry nvcr.dgxkey --docker-server=nvcr.io --docker-username=\$oauthtoken --docker-email=<email> --docker-password=<NGC API Key>
