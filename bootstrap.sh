604911bea645ea0b3fb92b7d37e130d5742f289c
JungWKim <lionmini@naver.com>
2023-05-14 20:31:54 +0900

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,7 +29,7 @@ sudo apt install -y python3-pip net-tools nfs-common whois xfsprogs
 
 # download deepops repository
 cd ~
-git clone https://github.com/NVIDIA/deepops.git -b release-22.04
+git clone https://github.com/NVIDIA/deepops.git -b 22.08
 
 # enable kubectl & kubeadm auto-completion
 echo "source <(kubectl completion bash)" >> ${HOME}/.bashrc
@@ -80,16 +80,14 @@ sed -i "s/kube_network_plugin: calico/kube_network_plugin: flannel/g" submodules
 sed -i "s/gpu_operator_preinstalled_nvidia_software: true/gpu_operator_preinstalled_nvidia_software: false/g" config/group_vars/k8s-cluster.yml
 # force install NVIDIA driver or gpu operator even if GPU not detected
 sed -i "s/nvidia_driver_force_install: false/nvidia_driver_force_install: true/g" config/group_vars/all.yml
-# change gpu operator image version from 1.10.0 to 1.11.1
-sed -i "s/gpu_operator_chart_version: \"1.10.0\"/gpu_operator_chart_version: \"1.11.1\"/g" roles/nvidia-gpu-operator/defaults/main.yml
+# change gpu operator image version from 1.11.1 to 1.11.1
+#sed -i "s/gpu_operator_chart_version: \"1.11.1\"/gpu_operator_chart_version: \"1.11.1\"/g" roles/nvidia-gpu-operator/defaults/main.yml
 # change gpu operator container runtime from containerd to docker
 sed -i "s/gpu_operator_default_runtime: \"containerd\"/gpu_operator_default_runtime: \"docker\"/g" roles/nvidia-gpu-operator/defaults/main.yml
-# change gpu operator driver version from 510.47.03 to 515.105.01
-sed -i "s/gpu_operator_driver_version: \"510.47.03\"/gpu_operator_driver_version: \"515.105.01\"/g" roles/nvidia-gpu-operator/defaults/main.yml
-# disable persistence mode
-#sed -i "s/nvidia_driver_persistence_mode_on: yes/nvidia_driver_persistence_mode_on: no/g" roles/galaxy/nvidia.nvidia_driver/defaults/main.yml
+# change gpu operator driver version from 515.48.07 to 515.105.01
+sed -i "s/gpu_operator_driver_version: \"515.48.07\"/gpu_operator_driver_version: \"515.105.01\"/g" roles/nvidia-gpu-operator/defaults/main.yml
 # skip reboot while installing nvidia driver
-sed -i "s/nvidia_driver_skip_reboot: no/nvidia_driver_skip_reboot: yes/g" roles/galaxy/nvidia.nvidia_driver/defaults/main.yml
+#sed -i "s/nvidia_driver_skip_reboot: no/nvidia_driver_skip_reboot: yes/g" roles/galaxy/nvidia.nvidia_driver/defaults/main.yml
 # change nvidia driver version from 510 to 470
 #sed -i "s/510/470/g" roles/galaxy/nvidia.nvidia_driver/defaults/main.yml
 
